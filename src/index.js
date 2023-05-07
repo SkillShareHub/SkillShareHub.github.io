aa3cbe2e86f5f8586b74623076efafb02c0fe487
usama <usamaahmad8000@gmail.com>
2023-05-07 16:29:44 +0500

    basicreact1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import changeTitleWhenUserMovesAway from './titleChanger';
+import changeTitleWhenUserMovesAway from '../src/tittleChanger';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
